09d799ee89028bbf6830c162951f3762a64479d1
mmorutwe <131787812+mmorutwe@users.noreply.github.com>
2024-11-06 12:28:59 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,8 @@
-
+variable "admin_username" {
+  description = "The admin username for the virtual machine."
+  type        = string
+}
+variable "admin_password" {
+  description = "The admin password for the virtual machine."
+  type        = string
+}
